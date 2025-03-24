d5396cc80b8465376ba1a4a8ae011cf38ab84697
huydodev <doquanghuy2711@gmail.com>
2025-03-25 01:02:42 +0700

    add top deal

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -31,3 +31,24 @@ dots.forEach((dot, i) => dot.onclick = () => updateSlide(index = i + 1));
 // Tự động chạy
 setInterval(() => updateSlide(++index), 3000);
 updateSlide();
+
+let currentPage = 1;
+let totalTranslateX = 0;
+
+function scrollTopDeal(direction) {
+  const list = document.querySelector('.top__cheap-list');
+  const slides = document.querySelectorAll('.top__cheap-item');
+  const btnLeft = document.querySelector('.top__cheap-btn--left');
+  const btnRight = document.querySelector('.top__cheap-btn--right');
+  const pages = Math.ceil(slides.length / 6);
+
+  totalTranslateX += direction === 'next' ? -100 : 100;
+  currentPage += direction === 'next' ? 1 : -1;
+
+  list.style.transition = "0.5s ease";
+  list.style.transform = `translateX(${totalTranslateX}%)`;
+
+  btnLeft.style.display = currentPage === 1 ? 'none' : 'block';
+  btnRight.style.display = currentPage === pages ? 'none' : 'block';
+}
+
