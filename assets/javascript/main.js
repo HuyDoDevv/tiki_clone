7759ca64c24dc0a975ec2e376527af1931559e3a
huydodev <doquanghuy2711@gmail.com>
2025-03-25 15:57:20 +0700

    add flash sale

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -32,8 +32,8 @@ dots.forEach((dot, i) => dot.onclick = () => updateSlide(index = i + 1));
 setInterval(() => updateSlide(++index), 3000);
 updateSlide();
 
-let currentPage = 1;
-let totalTranslateX = 0;
+let currentPageTopDeal = 1;
+let totalTranslateXTopDeal = 0;
 
 function scrollTopDeal(direction) {
   const list = document.querySelector('.top__cheap-list');
@@ -42,13 +42,34 @@ function scrollTopDeal(direction) {
   const btnRight = document.querySelector('.top__cheap-btn--right');
   const pages = Math.ceil(slides.length / 6);
 
-  totalTranslateX += direction === 'next' ? -100 : 100;
-  currentPage += direction === 'next' ? 1 : -1;
+  totalTranslateXTopDeal += direction === 'next' ? -100 : 100;
+  currentPageTopDeal += direction === 'next' ? 1 : -1;
 
   list.style.transition = "0.5s ease";
-  list.style.transform = `translateX(${totalTranslateX}%)`;
+  list.style.transform = `translateX(${totalTranslateXTopDeal}%)`;
 
-  btnLeft.style.display = currentPage === 1 ? 'none' : 'block';
-  btnRight.style.display = currentPage === pages ? 'none' : 'block';
+  btnLeft.style.display = currentPageTopDeal === 1 ? 'none' : 'block';
+  btnRight.style.display = currentPageTopDeal === pages ? 'none' : 'block';
 }
 
+
+
+let currentPageFlashSale = 1;
+let totalTranslateXFlashSale = 0;
+
+function scrollFlashSale (direction) {
+  const list = document.querySelector('.flash__sale-list');
+  const slides = document.querySelectorAll('.flash__sale-item');
+  const btnLeft = document.querySelector('.flash__sale-btn--left');
+  const btnRight = document.querySelector('.flash__sale-btn--right');
+  const pages = Math.ceil(slides.length / 6);
+
+  totalTranslateXFlashSale += direction === 'next' ? -100 : 100;
+  currentPageFlashSale += direction === 'next' ? 1 : -1;
+
+  list.style.transition = "0.5s ease";
+  list.style.transform = `translateX(${totalTranslateXFlashSale}%)`;
+
+  btnLeft.style.display = currentPageFlashSale === 1 ? 'none' : 'block';
+  btnRight.style.display = currentPageFlashSale === pages ? 'none' : 'block';
+}
